c4f7bc0722a5c3ed44cfb56697cc31b19c798b17
sherov-eldor <eldor.sherov.97@inbox.ru>
2023-03-04 22:19:04 +0500

    deleted comments

--- app/__init__.py
+++ app/__init__.py
@@ -22,41 +22,4 @@ with app.test_request_context():
     oneid.Set_Callback(url_for('get_params'))
 
 
-# app = Flask(__name__)
-# app.config.from_pyfile('config.py')
-# db = SQLAlchemy(app)
-
-# from app import route
-
-# with app.app_context():
-#     # db.drop_all()
-#     db.create_all()
-
-
-
-
-# from flask_oneid import OneID
-# from flask import *
-# def create_app():
-#     oneid = OneID()
-#     app = Flask(__name__)
-#     app.config.from_pyfile('config.py')
-#     oneid.init_app(app)
-    
-#     @app.route("/", methods=['GET'])
-#     def index():
-#         return "Hello World"
-#     @app.route("/params", methods=['GET'])
-#     def params():
-#         data = oneid.Params_To_Dict(request.args)
-#         return jsonify(data)
-    
-#     with app.test_request_context():
-#         oneid.Set_Callback(url_for('params'))
-#     return app
-
-# app = create_app()
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
 
