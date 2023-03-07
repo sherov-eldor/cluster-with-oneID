e889040e454b56ce67bf0483f3c0e355114fa1c5
sherov-eldor <eldor.sherov.97@inbox.ru>
2023-03-07 09:20:58 +0500

    added user profile

--- app/static/script.js
+++ app/static/script.js
@@ -5,18 +5,29 @@ $(document).ready(function () {
   $('.auth__byagrify').show()
   $('.btn--agrify').addClass('auth__button--active')
 
-  $('.btn--one_id').click(function(){
+  $('.btn--one_id').click(function () {
     $('.auth__byoneid').show()
     $('.auth__byagrify').hide()
     $('.btn--agrify').removeClass('auth__button--active')
     $('.btn--one_id').addClass('auth__button--active')
   })
 
-  $('.btn--agrify').click(function(){
+  $('.btn--agrify').click(function () {
     $('.auth__byoneid').hide()
     $('.auth__byagrify').show()
     $('.btn--one_id').removeClass('auth__button--active')
     $('.btn--agrify').addClass('auth__button--active')
   })
+
+  // BARS 
+
+  const $button = document.querySelector('#sidebar-toggle');
+  const $wrapper = document.querySelector('#wrapper');
+
+  $button.addEventListener('click', (e) => {
+    $('.profile_logo').toggleClass('hide')
+    e.preventDefault();
+    $wrapper.classList.toggle('toggled');
+  });
 
 });
